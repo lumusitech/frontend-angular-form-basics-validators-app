e14b2f6969e68b91cf8950224d5399298952b33e
lumusitech <lumusitech@gmail.com>
2023-11-12 18:03:42 -0300

    feat: add email validator

--- src/app/form-model/form-model.component.ts
+++ src/app/form-model/form-model.component.ts
@@ -1,45 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 
-import { debounceTime } from 'rxjs/operators'
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form-model',
   templateUrl: './form-model.component.html',
-  styleUrls: ['./form-model.component.sass']
+  styleUrls: ['./form-model.component.sass'],
 })
 export class FormModelComponent implements OnInit {
-
   form: UntypedFormGroup;
 
   constructor() {
     this.form = new UntypedFormGroup({
-      name: new UntypedFormControl("", [
+      name: new UntypedFormControl('', [
         Validators.required,
-        this.onlyLettersValidator
+        this.onlyLettersValidator,
       ]),
 
-      lastname: new UntypedFormControl("", [
+      lastname: new UntypedFormControl('', [
         Validators.required,
-        this.onlyLettersValidator
+        this.onlyLettersValidator,
       ]),
 
-      email: new UntypedFormControl("", [
-        Validators.required
+      email: new UntypedFormControl('', [
+        Validators.required,
+        Validators.email,
       ]),
 
-      password: new UntypedFormControl("", [
-        Validators.required
-      ]),
-      repeat_password: new UntypedFormControl("", [
-        Validators.required
-      ])
+      password: new UntypedFormControl('', [Validators.required]),
+      repeat_password: new UntypedFormControl('', [Validators.required]),
     });
   }
 
   ngOnInit(): void {
     const emailValidator = this.form.controls.email;
-    emailValidator.valueChanges.pipe(debounceTime(500)).subscribe(values => console.log(values));
+    emailValidator.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((values) => console.log(values));
   }
 
   onlyLettersValidator(form: any) {
@@ -51,28 +53,24 @@ export class FormModelComponent implements OnInit {
   onSubmit() {
     console.log(this.form.value);
     this.form = new UntypedFormGroup({
-      name: new UntypedFormControl("", [
+      name: new UntypedFormControl('', [
         Validators.required,
-        this.onlyLettersValidator
+        this.onlyLettersValidator,
       ]),
 
-      lastname: new UntypedFormControl("", [
+      lastname: new UntypedFormControl('', [
         Validators.required,
-        this.onlyLettersValidator
+        this.onlyLettersValidator,
       ]),
 
-      email: new UntypedFormControl("", [
-        Validators.required
+      email: new UntypedFormControl('', [
+        Validators.required,
+        Validators.email,
       ]),
 
-      password: new UntypedFormControl("", [
-        Validators.required
-      ]),
+      password: new UntypedFormControl('', [Validators.required]),
 
-      repeat_password: new UntypedFormControl("", [
-        Validators.required
-      ])
+      repeat_password: new UntypedFormControl('', [Validators.required]),
     });
   }
-
 }
